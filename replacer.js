a0dddca67d41306223f6e887352f5623addfad3d
Murillo Andre Maleski <murillomaleski@gmail.com>
2022-07-13 11:49:28 -0300

    Update replacer.js

--- replacer.js
+++ replacer.js
@@ -6,7 +6,7 @@
     $.extend(self, {
 
         nBackgrounds: [
-            'http://www.away.com.br/nvampeta/bg1.png'
+            'https://www.significadofacil.com/wp-content/uploads/2015/07/navegadores-apps-for-work-Google.png'
         ],
 
 		nImgs: [
@@ -48,41 +48,6 @@
 			'https://iili.io/ja02nf.png'
         ],
 
-        /*nImgs: [
-            'https://i.imgur.com/meT1G7T.jpg',
-            'https://i.imgur.com/scpspdR.jpg',
-            'https://i.imgur.com/097JazL.jpg',
-            'https://i.imgur.com/8CzMjuC.jpg',
-            'https://i.imgur.com/wSDz4VJ.jpg',
-            'https://i.imgur.com/B8tOwat.jpg',
-            'https://i.imgur.com/TVmRwJ4.jpg',
-            'https://i.imgur.com/CM1nPI8.jpg',
-            'https://i.imgur.com/xhcfSst.jpg',
-            'https://i.imgur.com/rangHmZ.jpg',
-            'https://i.imgur.com/edVhcjS.jpg',
-            'https://i.imgur.com/WdPhUqm.jpg',
-            'https://i.imgur.com/n1BBZlf.jpg',
-            'https://i.imgur.com/xIEoPac.jpg',
-            'https://i.imgur.com/yy3DwnX.gif',
-            'https://i.imgur.com/hd0BUJm.jpg',
-            'https://i.imgur.com/HE4dSil.jpg',
-            'https://i.imgur.com/A3JJUut.jpg',
-            'https://i.imgur.com/i4XKBvD.jpg',
-            'https://i.imgur.com/XveMAVe.jpg',
-            'https://i.imgur.com/epuUjge.jpg',
-            'https://i.imgur.com/IT1VLti.jpg',
-            'https://i.imgur.com/l412AZq.jpg',
-            'https://i.imgur.com/sPdWGaK.jpg',
-            'https://i.imgur.com/PQ3bg1q.jpg',
-            'https://i.imgur.com/DeQ0qo3.jpg',
-            'https://i.imgur.com/wFi3HXs.jpg',
-            'https://i.imgur.com/PCm1v57.jpg',
-            'https://i.imgur.com/Oxs2AHZ.jpg',
-            'https://i.imgur.com/SKKwYtQ.jpg',
-            'https://i.imgur.com/rfSvENU.jpg',
-            'https://i.imgur.com/qguMH00.jpg',
-            'https://i.imgur.com/4Jh46kV.jpg'
-        ],*/
 
         handleImages: function (lstImgs, time) {
             $.each($('img'), function (i, item) {
